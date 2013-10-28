880f674558061b17a1cac13003e9f99742798c55
Tom Gillespie <tgbugs@gmail.com>
2013-10-28 00:18:35 -0700

    combine the subjects tree with the steps tree, the steps tree means you should only have to give the root node for any given subject in an experiment, for now dont try to extend this to doccumenting the procedure for multiple subjects at the same time, that gets a bit complicated, DO need a way to look ahead to make sure conditions will be satisfied and not rely on some external protocol, eg make sure one pyramidal one som

--- database/steps.py
+++ database/steps.py
@@ -2,7 +2,7 @@
 
 #substeps or dependency tree to say a step is compelted? I think they are the same...
 #because let's say you want to do an analysis step, then the tree will say 'ok you need to do these things first'#and when it gets to a step that has other deps, it will go all the way to the first dependency and then work its way down
-class Step:
+class _Step:
     #XXX: NOTE these are only the DIRECT dependencies, and the order specifies only how they are to be run
     #not the actual order of steps which will be constructed from the dependencies
     #FIXME easy way to specify that a step should be depth or dredth first???? easy! just make a checkpoint step!
@@ -32,6 +32,99 @@ class Step:
         pass
     def success(self):
         pass
+
+
+class Step: #TODO make this peristable
+    dependencies={'set','set'}
+    preceeding_step='None' #TNIS could also be derived from a list? and is not necessarily a strict dependnecy, may ultimately go away?
+    #TODO searching for a way to combine topo sort with procedures that must be done by one person and therefore need some regularity
+
+    def noSideEffects(self):
+        #database read
+        #get value from datasource
+        #do analysis or strict functions
+        pass
+    def sideEffects(self):
+        #print to terminal! BUT this is ok, because THIS CODE doesn't do it, the datasource handles ALL of that
+        #write to database; minimal a boolean 'step succeeded/failed'
+        #change a variable; eg move a motor or set MCC state, clx file
+        #write analysis results
+        pass
+
+    #TODO problem: looks like we need to intersperse se with nse?
+
+class Step: #fucking mess... back to thinking
+    deps={'',''}
+    previous_step=None
+    datasources=['',''] #FIXME when a writer writes is it just another datasource???
+    reader_types=['',''] #should be ordered for consistency could be a set
+    writer_types=['','']
+    def __init__(self,ExpStepRecord,datasourcedict={}):
+        self.ExpStepRecord=ExpStepRecord
+        self.nodepreaders=nodpereaders #FIXME shouldn't this be defined before __init__ since we've split off datasources? actually I think it needs to be live! :/
+        self.Writers=Writers #big problem here... the 'writer' is an object that may not exist... since it would be an initilized mapped class :/
+    def readSomeThings(self,Readers=()):
+        #FIXME preferably read from memory, which I THINK is how Readers is flexible
+        #in that Writers can return the written thing directly to the reader... and
+        #the code that goes through the steps should automatically pass Writers to Readers
+        #directly from the dep tree, so we call self.go(nondepreaders,(dep1.go(),dep2.go(),dep3.go())) or something like that
+        pass
+    def doSomeThings(self):
+        pass
+    def writeSomeThings(self):
+        #FIXME shouldn't we be able to smartly pass values already in memory/session from step to step without having to read from the database?
+        pass
+    def go(self,Readers=(),Writers=()):
+        def readThings(Readers=()):
+            return [r.read for reader in  Readrs]
+        def doThings(readThings=()):
+        def writeThings(doneThings=()):
+
+
+class Step:
+    deps=[]
+    setters=[]
+    datasources=[]
+    analysisDatasources=[]
+    
+    def setValues(self):
+        pass
+    def getValues(self):
+        pass
+    def checkGottenAgainstSet(self):
+        pass
+
+
+class addNewSubjectStep: #??? instead of the pre/inter/post I use currently?
+    pass
+
+class trmGotCell:
+    datasources=['trmGetBool']
+    def do(self):
+        for ds in self.datasources:
+
+
+def doExperiment():
+    def getDataOnSubjectSet(): #this is what will trigger the dep tree to check and compile
+        return None
+    while 1:
+        getDataOnSubjectSet() #FIXME seems like we may still need a way to deal with the subject tree too...
+        if input()=='y': #a check to get a new slice? seems ugly compared to pre/inter/post...
+        changeHigherLevelSubject() #yeah... still have to be able to changes slices...
+        #how to doccument the pre/inter/post as part of the dep tree??? ACTUALLY, don't have too, it already IS doccumented, can just record that we did the steps again for a different set of subjects...
+        #hybrid of the subject tree and the dep tree...
+        #TODO a experiment-step-subject history table thing???!?!
+        #this is why dep trees only need to be for a single subject, experiment logic will handle the rest
+
+
+class stepStateTree: #replace with a list or a dict under exp logic?
+    """class to keep track of the true false state of upstream nodes"""
+    #FIXME this might already be present in the database, just check the step record
+    #and make sure there is an record already matching Experiment-StepName with a value of True
+    #surely there is a faster way... well yeah, just store the Step records (association table between experiment and step and time and maybe one other thing since steps can repeate) to a list in memory once they are persisted and then just check that so we don't have to query
+
+
+
 
 
 #maybe start with an ordered list and then reorder based on deps?
