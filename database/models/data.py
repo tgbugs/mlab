d1f629bd86b83c512a15f44ee677c2a0295a7d89
Tom Gillespie <tgbugs@gmail.com>
2014-01-02 17:35:24 -0800

    a few small changes to data, specifally switching to the standard note helper class

--- database/models/data.py
+++ database/models/data.py
@@ -297,7 +297,7 @@ class Repository(Base):
         return super().strHelper(depth=depth,attr='url')
 
 
-class File(fHasNotes, HasProperties, Base): #REALLY GOOD NEWS: in windows terminal drag and drop produces filename! :D
+class File(HasNotes, HasProperties, Base): #REALLY GOOD NEWS: in windows terminal drag and drop produces filename! :D
     """class for interfacing with things stored outside the database, whether datafiles or citables or whatever"""
     #TODO references to a local file should be replaced with a reference to that computer so that on retrieval if the current computer does not match we can go find other repositories for the same file damn it this is going to be a bit complicated
     #ideally the failover version selection should be ordered by retrieval time and should be completely transparent
