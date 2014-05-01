faf9c8090d80a253199808e7a6f49a05585f4bc3
Tom Gillespie <tgbugs@gmail.com>
2014-05-01 14:08:05 -0700

    notes

--- rig/inferno.py
+++ rig/inferno.py
@@ -48,6 +48,34 @@ class danteFuncs(clxFuncs,mccFuncs):
 
         return None
 
+    def click_protocol_button(self,button=None):
+        #window name is 'Clampex - some shit'
+        #gonna have to enum windows
+        for i,n in get_windows():
+            if n.count('Clampex'):
+                name=n
+                break
+        #get the window position
+
+        #604,8
+        #y position of the row 148
+        #x width of buttons 656-683
+        #8 buttons starting at 670
+        #3 at 910
+        #3 at 1005
+        #3 at 1100
+
+
+        pass
+
+    def click_record(self):
+
+        #604,8 -> 953,76
+        #left and top
+        pass
+
+
+
 
     def DOALLTHETHINGS(hsToCellDict,csvPath): #FIXME eh, given the structure of this program, just stuff these in at init
         stuff=None
@@ -56,14 +84,31 @@ class danteFuncs(clxFuncs,mccFuncs):
         #FIXME may need a way to stop execution in the middle???
 
 
-        #load mcc state
-        #load protocol
+        #set the mode for each headstage NOTHING ELSE
+        #load protocol <- use the mouse click
         #wait for key input or cancel
-        #record mcc state, protocol name, and cells
-        #run protocol
+        #record mcc state, protocol, and cell asociations
+        #run protocol <- use the mouse click
+        #get the new filename
+        #print text to terminal
 
+        hsdict={
+            'labels'=['gen from output format?']
+            1={'cell id':'aa',}
+        }
         return None
 
+#output format
+
+#filename format YYYY_MM_DD_nnnn
+#protocol p1 p2 p3 p4 corrisponding to the buttons
+
+#nnnn   p1
+#headstage  1   2   3   4
+#cell id    a   b   c   d 
+#mode       vc  ic  vc  ic #map between mcc + channel and the digitizer input channel
+#holding    -70 OFF OFF -70 #OFF for holding disabled
+#bridge balance
 
 #structure for associating protocols to mcc settings
 
